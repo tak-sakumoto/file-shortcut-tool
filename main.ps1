5299ac08e7cee9ad33e847379f00d4e11d14ff0b
Sakumoto Takuya <tak.sakumoto@gmail.com>
2023-09-23 14:24:00 +0900

    Support for getting the names of shortcut files

--- main.ps1
+++ main.ps1
@@ -13,10 +13,24 @@ $data = Import-Csv -Path $listPath
 # Create shortcuts for each path
 foreach ($line in $data) {
     $targetPath = $line.Path
+
+    # Get the parent of the shortcut if it is listed in the CSV file
     $parent = $line.Parent
+    # Get the default parent of the shortcut if it is not listed in the CSV file
     if([string]::IsNullOrEmpty($parent)){
         $parent = $defaultParent
     }
-    $shortcutPath = $parent + "\" + (Split-Path -Leaf $targetPath) + ".lnk"
+
+    # Get the file name if it is listed in the CSV file
+    $name = $line.Name
+    # Get the leaf of the target path if it is not listed in the CSV file
+    if([string]::IsNullOrEmpty($name)){
+        $name = (Split-Path -Leaf $targetPath)
+    }
+
+    # Get the shortcut path
+    $shortcutPath = $parent + "\" + $name + ".lnk"
+
+    # Create the shoutcut
     New-Shortcut -targetPath $targetPath -shortcutPath $shortcutPath
 }
